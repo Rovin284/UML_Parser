022dcc7b71cc0621e10fa53f2fc1985bddb93989
Rovin Patwal <rovinpatwal@Rovins-MacBook-Pro.local>
2017-03-24 00:39:47 -0700

    Commit

--- Dev/JavaParser.java
+++ Dev/JavaParser.java
@@ -1,10 +1,10 @@
 class JavaParser {
-    //Reference Code for AST Abstract Syntax Tree/*
+    //Reference Code for AST Abstract Syntax Tree
     public ASTNode Test{
 	CompilationUnit compilationUnit = JavaParser.parse("class A { }");
 	ClassOrInterfaceDeclaration classA = compilationUnit.getClassByName("A");
     }
-*/
+
     public static void main(String[] args) {
         System.out.println("Java Parser!"); 
     }
