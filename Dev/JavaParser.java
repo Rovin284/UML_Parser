2a4b0ac5a2d7455600485d8d0810b3d8d0074298
Rovin Patwal <rovinpatwal@Rovins-MacBook-Pro.local>
2017-03-28 02:07:06 -0700

    Updated

--- Dev/JavaParser.java
+++ Dev/JavaParser.java
@@ -1,11 +1,15 @@
 class JavaParser {
     //Reference Code for AST Abstract Syntax Tree
-    public ASTNode Test{
-	CompilationUnit compilationUnit = JavaParser.parse("class A { }");
-	ClassOrInterfaceDeclaration classA = compilationUnit.getClassByName("A");
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Java Parser!"); 
-    }
+	public ASTNode Test{
+		CompilationUnit compilationUnit = JavaParser.parse("class A { }");
+		ClassOrInterfaceDeclaration classA = compilationUnit.getClassByName("A");
+    	}
+	
+	private String getClassName(String code){
+		//Code for getting class name
+	}
+	
+	public static void main(String[] args) {
+        	System.out.println("Java Parser!"); 
+    	}
 }
