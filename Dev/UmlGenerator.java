9033457aad7ab4f72a9e6893288f3e14e9cbffeb
Rovin Patwal <rovinpatwal@Rovins-MacBook-Pro.local>
2017-03-31 12:38:12 -0700

    Updated the code

--- Dev/UmlGenerator.java
+++ Dev/UmlGenerator.java
@@ -1,5 +1,8 @@
 class UmlGenerator {
-    public static void main(String[] args) {
-        System.out.println("Uml Gnerator!!!");
-    }
+    	public static Boolean generatePNG(String grammar, String outPath) {
+		//code for generator
+	}
+	public static void main(String[] args) {
+        	System.out.println("Uml Gnerator!!!");
+    	}
 }
