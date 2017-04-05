c00bb4edc23c2e05df21db1e94297ddba7f85b97
Rovin Patwal <rovinpatwal@Rovins-MacBook-Pro.local>
2017-04-05 13:07:44 -0700

    Updated the code

--- Dev/UmlGenerator.java
+++ Dev/UmlGenerator.java
@@ -1,7 +1,11 @@
 class UmlGenerator {
     	boolean grammarFlag = true;
+	String grammar;
 	public static Boolean generatePNG(String grammar, String outPath) {
 		//code for generator
+		this.grammar = grammar;
+		String webLink = "http://yuml.me/diagram/plain/class/" + grammar
+                    + ".png";
 	}
 	public static void main(String[] args) {
         	System.out.println("Uml Gnerator!!!");
