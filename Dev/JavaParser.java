4f6695d4a30955ac6102eb80ee301182c7bdc2ca
Rovin Patwal <rovinpatwal@Rovins-MacBook-Pro.local>
2017-04-14 09:08:44 -0700

    Updated the code

--- Dev/JavaParser.java
+++ Dev/JavaParser.java
@@ -4,14 +4,6 @@ class JavaParser {
 	boolean codeFlag = true;
 	final String inputPath;
 	final String outputPath;
-	public JavaParser(String inputPath, String outputPath){
-		this.inputPath = inputPath;
-		this.outputPath = inputPath + "\\Output\\" +  outputPath +".png";
-	}
-	public ASTNode Test{
-		CompilationUnit compilationUnit = JavaParser.parse("class A { }");
-		ClassOrInterfaceDeclaration classA = compilationUnit.getClassByName("A");
-    	}
 		
 	private String getDependency(String code){
                 //Code for getting dependency between classes
@@ -19,19 +11,6 @@ class JavaParser {
 
 	private String getMultiplicity(String code){
                 //Code for getting multiplicity relationship
-        }	
-	private String getClassName(String code){
-		//Code for getting class name
-	}
-
-	private String getMethods(String code){
-                //Code for getting Method names
-        }	
-	private String getAttributes(String code){
-                //Code for getting Method names
-        }
-	private String getInterface(String code){
-                //Code for getting Method names
         }	
 	public static void main(String[] args) {
         	System.out.println("Java Parser!"); 
